60478edb0fe8ef81b3f0f73e9acaaed72801bb32
Константин Васильев <89670253660@mail.ru>
2021-12-14 19:54:59 +0300

    Не стреляйте в пианиста он играет как умеет

--- homework.py
+++ homework.py
@@ -1,22 +1,39 @@
 from dataclasses import dataclass
-from typing import Callable, Dict, List
+from typing import Dict, List, Type
 
 
 @dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    training_type: str
-    duration: float
-    distance: float
-    speed: float
-    calories: float
+    def __init__(self,
+                 training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float
+                 ) -> None:
+
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def __repr__(self) -> str:
+        return (
+            'Тип тренировки: {training_type}; '
+            'Длительность: {duration:.3f} ч.; '
+            'Дистанция: {distance:.3f} км; '
+            'Ср. скорость: {speed:.3f} км/ч; '
+            'Потрачено ккал: {calories:.3f}.'
+        )
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return repr(self).format(
+            training_type=self.training_type, duration=self.duration,
+            distance=self.distance, speed=self.speed,
+            calories=self.calories
+        )
 
 
 class Training:
@@ -49,29 +66,35 @@ class Training:
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
 
-        return InfoMessage(self.__class__.__name__,
-                           self.duration,
-                           self.get_distance(),
-                           self.get_mean_speed(),
-                           self.get_spent_calories())
+        return InfoMessage(
+            self.__class__.__name__,
+            self.duration,
+            self.get_distance(),
+            self.get_mean_speed(),
+            self.get_spent_calories()
+        )
 
 
 class Running(Training):
     """Тренировка: бег."""
-    coeff_calorie_given_in_assignment_1: int = 18
-    coeff_calorie_given_in_assignment_2: int = 20
+    COEFF_CALORIE_1: int = 18
+    COEFF_CALORIE_2: int = 20
 
     def get_spent_calories(self) -> float:
-        return ((self.coeff_calorie_given_in_assignment_1
-                * super().get_mean_speed()
-                - self.coeff_calorie_given_in_assignment_2) * self.weight
-                / self.M_IN_KM * self.duration * self.MIN_IN_HOUR)
+        return (
+            (
+                self.COEFF_CALORIE_1 * super().get_mean_speed()
+                - self.COEFF_CALORIE_2
+            )
+            * self.weight / self.M_IN_KM
+            * self.duration * self.MIN_IN_HOUR
+        )
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    coeff_calorie_given_in_assignment_1: float = 0.035
-    coeff_calorie_given_in_assignment_2: float = 0.029
+    COEFF_CALORIE_1: float = 0.035
+    COEFF_CALORIE_2: float = 0.029
 
     def __init__(self,
                  action: int,
@@ -84,17 +107,21 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
 
-        return ((self.coeff_calorie_given_in_assignment_1 * self.weight
+        return (
+            (
+                self.COEFF_CALORIE_1 * self.weight
                 + (super().get_mean_speed() ** 2 // self.height)
-                * self.coeff_calorie_given_in_assignment_2 * self.weight)
-                * self.duration * self.MIN_IN_HOUR)
+                * self.COEFF_CALORIE_2 * self.weight
+            )
+            * self.duration * self.MIN_IN_HOUR
+        )
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38
-    coeff_calorie_given_in_assignment_1: float = 1.1
-    coeff_calorie_given_in_assignment_2: int = 2
+    COEFF_CALORIE_1: float = 1.1
+    COEFF_CALORIE_2: int = 2
 
     def __init__(self,
                  action: int,
@@ -102,37 +129,44 @@ class Swimming(Training):
                  weight: float,
                  length_pool: int,
                  count_pool: int
-                 ):
+                 ) -> None:
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
-        return (self.length_pool * self.count_pool / self.M_IN_KM
-                / self.duration)
+        return (
+            self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        )
 
     def get_spent_calories(self) -> float:
-        return ((self.get_mean_speed()
-                + self.coeff_calorie_given_in_assignment_1)
-                * self.coeff_calorie_given_in_assignment_2 * self.weight)
+        return (
+            (self.get_mean_speed() + self.COEFF_CALORIE_1)
+            * self.COEFF_CALORIE_2 * self.weight
+        )
 
 
 def read_package(workout_type: str, data: List) -> Training:
     """Прочитать данные полученные от датчиков."""
-    workouts: Dict[str, Callable] = {'SWM': Swimming,
-                                     'RUN': Running,
-                                     'WLK': SportsWalking}
+    workouts: Dict[str, Type] = {
+        'SWM': Swimming,
+        'RUN': Running,
+        'WLK': SportsWalking
+    }
 
     try:
         return workouts[workout_type](*data)
-    except ValueError:
+    except KeyError:
         print('Неизвестный тип тренировки')
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    info: InfoMessage = training.show_training_info()
-    print(info.get_message())
+    try:
+        info: InfoMessage = training.show_training_info()
+        print(info.get_message())
+    except AttributeError:
+        print('', end='')
 
 
 if __name__ == '__main__':
